aea10c555144b029e9a7a5cf2b17cebaa670e655
catalina-acosta <catalina.acosta.rivera@gmail.com>
2024-12-19 15:31:54 +0100

    basic file structure built and icons added

---
+++ db.js
@@ -0,0 +1,122 @@
+let myDishes = [
+    {
+    "name": "Frühlingsrollen mit Gemüse",
+    "price": 4.50,
+    "description": "Knusprige Frühlingsrollen, gefüllt mit frischem Gemüse und Glasnudeln, serviert mit süß-saurer Sauce.",
+    "mainDish": false
+    },
+    {
+    "name": "Edamame mit Meersalz",
+    "price": 3.90,
+    "description": "Gedämpfte grüne Sojabohnen, leicht mit Meersalz bestreut.",
+    "mainDish": false
+    },
+    {
+    "name": "Gyoza mit Hühnerfüllung",
+    "price": 5.20,
+    "description": "Gedämpfte und gebratene Teigtaschen mit saftiger Hühnerfüllung, dazu Sojasauce.",
+    "mainDish": false
+    },
+    {
+    "name": "Wan-Tan-Suppe",
+    "price": 5.90,
+    "description": "Aromatische Brühe mit gefüllten Wan-Tan-Teigtaschen und Frühlingszwiebeln.",
+    "mainDish": false
+    },
+    {
+    "name": "Knusprige Tempura-Garnelen",
+    "price": 6.50,
+    "description": "Goldbraun gebackene Garnelen im Tempura-Teig, serviert mit Teriyaki-Dip.",
+    "mainDish": false
+    },
+    {
+    "name": "Scharfe Gurken-Pickles",
+    "price": 3.80,
+    "description": "Leicht scharfe, eingelegte Gurkenscheiben mit Sesam und Chiliöl.",
+    "mainDish": false
+    },
+    {
+    "name": "Miso-Suppe",
+    "price": 3.50,
+    "description": "Klassische Miso-Suppe mit Seetang, Tofu und Frühlingszwiebeln.",
+    "mainDish": false
+    },
+    {
+    "name": "Rindfleisch Teriyaki",
+    "price": 14.90,
+    "description": "Zartes Rindfleisch in Teriyaki-Sauce, serviert mit Jasminreis und gedünstetem Gemüse.",
+    "mainDish": true
+    },
+    {
+    "name": "Hühnercurry mit Kokosmilch",
+    "price": 12.50,
+    "description": "Mild-würziges Curry mit Hühnerfleisch, Kokosmilch, Gemüse und Reis.",
+    "mainDish": true
+    },
+    {
+    "name": "Pad Thai mit Garnelen",
+    "price": 13.90,
+    "description": "Gebratene Reisnudeln mit Garnelen, Ei, Erdnüssen und frischem Koriander.",
+    "mainDish": true
+    },
+    {
+    "name": "Gebratener Reis mit Gemüse",
+    "price": 10.90,
+    "description": "Aromatisch gebratener Reis mit knackigem Gemüse und asiatischen Gewürzen.",
+    "mainDish": true
+    },
+    {
+    "name": "Ente Süß-Sauer",
+    "price": 15.50,
+    "description": "Knusprige Ente mit süß-saurer Sauce, serviert mit Ananas und Paprika.",
+    "mainDish": true
+    },
+    {
+    "name": "Szechuan-Lamm",
+    "price": 16.50,
+    "description": "Würziges Lammfleisch mit Szechuan-Pfeffer und Paprika, dazu Reis.",
+    "mainDish": true
+    },
+    {
+    "name": "Gemüse-Teriyaki-Bowl",
+    "price": 11.50,
+    "description": "Frisches Gemüse in Teriyaki-Sauce, serviert mit Reis oder Quinoa.",
+    "mainDish": true
+    },
+    {
+    "name": "Sushi-Platte Deluxe",
+    "price": 22.90,
+    "description": "Eine Auswahl an Nigiri, Maki und Sashimi, serviert mit Wasabi und Sojasauce.",
+    "mainDish": true
+    },
+    {
+    "name": "Gebratene Nudeln mit Ente",
+    "price": 14.50,
+    "description": "Asiatische Eiernudeln, gebraten mit knuspriger Ente und Gemüse.",
+    "mainDish": true
+    },
+    {
+    "name": "Rotes Thai-Curry mit Tofu",
+    "price": 11.90,
+    "description": "Würziges rotes Curry mit Tofu, Gemüse und Kokosmilch, dazu Reis.",
+    "mainDish": true
+    },
+    {
+    "name": "Hühnerspieße Satay",
+    "price": 13.50,
+    "description": "Gegrillte Hühnerspieße mit Erdnusssauce und Jasminreis.",
+    "mainDish": true
+    },
+    {
+    "name": "Udon-Nudelsuppe mit Rindfleisch",
+    "price": 14.90,
+    "description": "Herzhafte Suppe mit dicken Udon-Nudeln, Rindfleisch und Frühlingszwiebeln.",
+    "mainDish": true
+    },
+    {
+    "name": "Knusprige Tofu-Würfel mit Sesam",
+    "price": 12.50,
+    "description": "Knusprig gebratene Tofu-Würfel in einer leichten Sesam-Sauce, serviert mit Gemüse und Reis.",
+    "mainDish": true
+    }
+];
