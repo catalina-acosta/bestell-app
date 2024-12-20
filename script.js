2f0a6ec99600f33d7a3d0cc2930833848f079d97
catalina-acosta <catalina.acosta.rivera@gmail.com>
2024-12-20 15:45:37 +0100

    added amount and isInBasket keys to objects. Created renderBasket(), renderBasketTotal(), basketDishTemplate and basketTotalTemplate

--- script.js
+++ script.js
@@ -29,8 +29,69 @@ function dishTemplate(index) {
 }
 
 function addToBasket(index) {
-    myBasket.push(myDishes[index]);
+    myDishes[index]["isInBasket?"] = true;
+    myDishes[index].amount ++;
+    renderBasket();
+}
+
+function renderBasket() {
+    let basketContentRef = document.getElementById("basketContent");
+    basketContentRef.innerHTML = "";
+    for (let item = 0; item < myDishes.length; item++) {
+        if (myDishes[item]["isInBasket?"] == true) {
+            basketContentRef.innerHTML += basketDishTemplate(item);
+        }
+        
+    }
+    renderBasketTotal();
+}
+
+function basketDishTemplate(item) {
+    return `
+            <div class="basket-card">
+                <h3>${myDishes[item].name}</h3>
+                <div class="quantity">
+                    <i class="fa-solid fa-minus"></i>
+                    <p>${myDishes[item].amount}</p>
+                    <i class="fa-solid fa-plus"></i>
+                    <p>${myDishes[item].price}</p>
+                    <i class="fa-solid fa-trash-can"></i>
+                </div>
+            </div>
+            `
+}
+
+function renderBasketTotal() {
+    basketTotalRef = document.getElementById("basketTotal");
+    let sumItems = 0;
+    let deliveryCosts = 5;
+
+    for (let index = 0; index < myDishes.length; index++) {
+        if (myDishes[index]["isInBasket?"] == true) {
+            let itemPrice = myDishes[index].price;
+            let itemAmount = myDishes[index].amount; 
+            sumItems += itemPrice * itemAmount;
+        }
+    }
+    basketTotalRef.innerHTML = basketTotalTemplate(sumItems, deliveryCosts);
 
 }
 
-// build basket counter function that counts how many of one item are in the basket. maybe if ? 
+function basketTotalTemplate(sumItems, deliveryCosts){
+    return `
+            <div class="basket-total">
+                <div class="subtotal">
+                    <p>Zwischensumme</p>
+                    <p>${sumItems}</p>
+                </div>
+                <div class="delivery-costs">
+                    <p>Lieferkosten</p>
+                    <p>${deliveryCosts}</p>
+                </div>
+                <div class="total">
+                    <h3>Gesamt</h3>
+                    <h3>${sumItems + deliveryCosts}</h3>
+                </div>
+            </div>        
+    `
+}
